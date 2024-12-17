1d85806141bbba949aad5e527de43b5aa0c4dc0f
adil-hlk <hallouk.adil@gmail.com>
2024-12-17 12:00:38 +0100

    chat en direct link css

--- php/phpChatTempsreel/header.php
+++ php/phpChatTempsreel/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat en direct</title>
-    <link reel="stylesheet" type="text/css" href="asset/style.css">
+    <link reel="stylesheet" type="text/css" href="asset/css/app.css">
     <link reel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 </head>
