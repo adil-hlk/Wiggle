d8e88bd204682a7ac5f5763fef242f7e59c0edf1
Vincent <232041@student.isfsc.be>
2024-12-17 12:38:36 +0100

    Menu

--- header.php
+++ header.php
@@ -35,7 +35,8 @@
             <ul class="nav-links">
                 <li><a href="<?php echo home_url('chercher-un-sitter'); ?>">Chercher un sitter</a></li>
                 <li><a href="<?php echo home_url('devenir-un-sitter'); ?>">Devenir un sitter</a></li>
-                <li><a href="se-connecter.html">Se connecter/S'inscrire</a></li>
+                <li><a href="se-connecter.html">Se connecter</a></li>
+                <li><a href="se-connecter.html">S'inscrire</a></li>
                 <li><a href="<?php echo home_url('aide'); ?>">FAQ/Contact</a></li>
             </ul>
     
