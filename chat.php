a0ff42d3da8bb36ce7092c38916fadd0e895555e
adil-hlk <hallouk.adil@gmail.com>
2024-12-18 12:06:36 +0100

    refonte chat box

--- chat.php
+++ chat.php
@@ -2,22 +2,7 @@
  get_header(); 
 ?>
 
-<div class="chat-box">
-  <div class="chat-header">
-    <h3>Chat</h3>
-  </div>
-  <div class="chat-messages">
-    <!-- Les messages seront ajoutés ici dynamiquement -->
-  </div>
-  <form class="chat-input" id="chatForm">
-    <input
-      type="text"
-      id="chatMessage"
-      placeholder="Écrivez un message..."
-      required
-    />
-    <button class="btn-rechercher md-2" type="submit">Envoyer</button>
-  </form>
-</div>
+<?php if (function_exists('simple_ajax_chat')) simple_ajax_chat(); ?>
+
 
 <?php get_footer(); ?>
