d7b02469bd7ce31b37b1b3e66acea3ef579aa180
Vincent <232041@student.isfsc.be>
2024-12-18 15:22:11 +0100

    début liste des profils

---
+++ liste_profil.php
@@ -0,0 +1,41 @@
+<?php 
+/* Template Name: liste des profils */
+get_header(); 
+?>
+
+<main>
+    <section class="main bg-custom">
+        <div class="container-cards">
+            <div class="card" id="c1">
+                <img class="illu-img-petit" src="assets/images/loupe.svg" alt="loupe (trouver une pdp)">
+                <p class="fw-bold">Christophe</p>
+                <p>***** (notation)</p>
+                <p>J'ai été agréablement surpris par la gentillesse de mon sitter</p>
+            </div>
+            <div class="card" id="c2">
+                <img class="illu-img-petit" src="assets/images/loupe.svg" alt="loupe (trouver une pdp)">
+                <p class="fw-bold">Christophe</p>
+                <p>***** (notation)</p>
+                <p>J'ai été agréablement surpris par la gentillesse de mon sitter</p>
+            </div>
+            <div class="card" id="c3">
+                <img class="illu-img-petit" src="assets/images/loupe.svg" alt="loupe (trouver une pdp)">
+                <p class="fw-bold">Christophe</p>
+                <p>***** (notation)</p>
+                <p>J'ai été agréablement surpris par la gentillesse de mon sitter</p>
+            </div>
+            <div class="card" id="c4">
+                <img class="illu-img-petit" src="assets/images/loupe.svg" alt="loupe (trouver une pdp)">
+                <p class="fw-bold">Christophe</p>
+                <p>***** (notation)</p>
+                <p>J'ai été agréablement surpris par la gentillesse de mon sitter</p>
+            </div>
+        </div>
+    </section>
+    
+    <section>
+        <img class="illu-img-moyen" src="<?php echo get_template_directory_uri(); ?>/assets/images/chat-couché.svg" alt="chat allongé"/>
+    </section>
+</main>
+
+<?php get_footer(); ?>
