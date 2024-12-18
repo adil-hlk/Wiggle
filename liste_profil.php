8e4ef087502cfa43e1bc4ce0dc4b635360d9fbb3
Vincent <232041@student.isfsc.be>
2024-12-18 15:28:49 +0100

    liste de profils

--- liste_profil.php
+++ liste_profil.php
@@ -36,6 +36,24 @@ get_header();
     <section>
         <img class="illu-img-moyen" src="<?php echo get_template_directory_uri(); ?>/assets/images/chat-couché.svg" alt="chat allongé"/>
     </section>
+
+    <div class="container">
+    <?php if (have_posts()): ?>
+      <h1>Mes articles</h1>
+
+      <ul>
+        <?php while(have_posts()): the_post(); ?>
+          <li>
+            <?php the_title() ?> - <?php the_author(); ?>
+            <a href="<?php the_permalink(); ?>">lire l'article</a>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+    <?php else: ?>
+      <h1>Aucun articles disponible pour le moment</h1>
+    <?php endif; ?>
+</div>
+
 </main>
 
 <?php get_footer(); ?>
