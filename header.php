1475d4a672637f1d0ada1585b8eff7d2a7b06221
adil-hlk <hallouk.adil@gmail.com>
2024-12-20 14:45:15 +0100

    menu-burger fonctionnel

--- header.php
+++ header.php
@@ -8,14 +8,7 @@
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
   <?php wp_head(); ?>
-      <script>
-        const burgerMenu = document.getElementById('burgerMenu');
-        const navLinks = document.querySelector('.nav-links');
-    
-        burgerMenu.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-        });
-    </script>
+      
 </head>
             
 <body <?php body_class(); ?>>
@@ -43,16 +36,25 @@
                     <li><a href="<?php echo home_url('mon-profil'); ?>">Mon Profil</a></li>
                 <?php endif; ?>
                 <li><a href="<?php echo home_url('aide'); ?>">FAQ/Contact</a></li>
-            </ul>
-            
-            <!-- Connecté ou pas -->
+                  <!-- Connecté ou pas -->
             <?php if (is_user_logged_in()): // si je suis connecté ?>
             <a href="<?php echo wp_logout_url(home_url('/')); // lien généré par wordpress pour déconnexion ?>">Déconnexion</a>
             <?php endif; ?>
+            </ul>
+            
+          
 
             <!-- Menu burger -->
             <div class="burger-menu" id="burgerMenu">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/menu-plein.svg" alt="Menu"/>
             </div>
+            <script>
+        const burgerMenu = document.getElementById('burgerMenu');
+        const navLinks = document.querySelector('.nav-links');
+    
+        burgerMenu.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    </script>
         </nav>
     </header>
